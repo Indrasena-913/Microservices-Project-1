400304d5787571f13d6d16291288690726e42b2b
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-23 20:09:27 +0530

    added refreshtoken to cookie

--- auth-service/app/api/routes/login.py
+++ auth-service/app/api/routes/login.py
@@ -4,7 +4,7 @@ from app.schemas.auth import LoginRequest,CreateaccessTokenRequest
 from app.api.dependency import db_dependency
 from app.models.user import User
 from app.core.security import verify_the_password
-from fastapi import HTTPException
+from fastapi import HTTPException,Response
 from app.services.auth import create_access_token,create_refresh_token
 
 
@@ -12,7 +12,7 @@ router=APIRouter()
 
 
 @router.post("/login",status_code=status.HTTP_201_CREATED)
-async def register_user(userdata:LoginRequest,db:db_dependency):
+async def register_user(userdata:LoginRequest,db:db_dependency,response:Response):
     existing_user=db.query(User).filter(User.email==userdata.email).first()
     if not existing_user:
         raise HTTPException(status_code=404,detail="User not found please register first")
@@ -30,9 +30,18 @@ async def register_user(userdata:LoginRequest,db:db_dependency):
     access_token=create_access_token(token_data)
     refresh_token=create_refresh_token(token_data)
 
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token["refresh_token"],
+        httponly=True,
+        secure=True,         
+        samesite="none",      
+        max_age=60 * 60 * 24 * 7, 
+        path="/"
+    )
+
     return {
     "access_token": access_token["access_token"],
-    "refresh_token": refresh_token["refresh_token"],
     "token_type": "bearer"
 }
 
